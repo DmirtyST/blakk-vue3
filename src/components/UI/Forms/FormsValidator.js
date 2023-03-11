5194c87f7d1c45cb2755e6b94f663b8bf3e4dacf
Dmitriy Stepchenkov <endless1995@ukr.net>
2023-03-11 23:23:36 +0100

    w

--- src/components/UI/Forms/FormsValidator.js
+++ src/components/UI/Forms/FormsValidator.js
@@ -4,13 +4,13 @@ const validNumber = helpers.regex(/(?=.*\+[0-9]{3}\s?[0-9]{2}\s?[0-9]{3}\s?[0-9]
 export const rules = computed(() => ({
     name: {
         required:helpers.withMessage('Обязательно для заполнения',required),
-        maxLength: helpers.withMessage('Максимально количество символов - 20',maxLength(20)),
+        maxLength: helpers.withMessage('Максимальное количество символов - 20',maxLength(20)),
         minLength: helpers.withMessage('Минимальное количество символов - 5',minLength(5)),
       },
     number:{
         required:helpers.withMessage('Обязательно для заполнения',required),
-        maxLength: helpers.withMessage('Введите коректный номер',maxLength(14)),
-        regexp: helpers.withMessage('Введите коректный номер',validNumber),
+        maxLength: helpers.withMessage('Введите корректный номер',maxLength(14)),
+        regexp: helpers.withMessage('Введите корректный номер',validNumber),
 
     }
 }))
